cafb61acc3cd8676fc20c04673bc542d19138fd1
abdal-security-group <abdal.group@gmail.comgit>
2023-05-30 01:35:28 +0330

    2.5

---
+++ abdal_net_py/abdal_net_py_admin.py
@@ -0,0 +1,10 @@
+"""
+ # Project Name: Blockchain Based Decentralized Anti Cyber Attacks System
+ # Programmer: Ebrahim Shafiei
+ # Programmer WebSite: https://hackers.zone/
+ # Programmer Email: Prof.Shafiei@Gmail.com
+ # License : MIT
+ # Current Date : 2023-05-30
+ # Current Time : 12:51 AM
+ # File Description: no description
+"""
